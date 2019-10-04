83652738523c6a7ffc8621c22214ee27e8b57dc7
bluetoothfx <firebold42@gmail.com>
2019-10-04 15:04:24 +0600

    added function calling

--- main.go
+++ main.go
@@ -6,4 +6,23 @@ import (
 
 func main() {
 	fmt.Println("Hello world")
+	result := add(5, 6)
+	result2 := addtion(1, 6)
+	fmt.Println(result)
+	fmt.Println(result2)
+}
+
+//calling a function
+func add(x int, y int) int {
+	return x + y
+}
+
+//another way of calling func
+func addtion(x, y int) int {
+	return x + y
+}
+
+//function returning muiltiple value
+func swap(x, y string) (string, string) {
+	return y, x
 }
