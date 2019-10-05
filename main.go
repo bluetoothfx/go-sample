3c579b61ad9fe226a26d34c76c82fdf406f5e09f
bluetoothfx <firebold42@gmail.com>
2019-10-05 06:56:48 +0600

    switch case example

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/cmplx"
+	"runtime"
 )
 
 //package level variable
@@ -10,6 +12,11 @@ var c, python, java bool
 
 //initialization of variables
 var ice, jock int = 1, 2
+
+//this is  a constant
+const (
+	Pi = 3.14
+)
 
 //bunch of variable declaration
 var (
@@ -24,7 +31,8 @@ func main() {
 	//result2 := addtion(1, 6)
 	//fmt.Println(result)
 	//fmt.Println(result2)
-	variablewithrange()
+	//constants()
+	switchCase()
 }
 
 //calling a function
@@ -91,4 +99,63 @@ func typeInference() {
 	complex := 0.867 + 0.5i // complex128 (this variable will be typed based on precision of the constant)
 
 	fmt.Println(i, j, iceream, fork, complex)
+}
+
+func constants() {
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
+
+//Control Flow Section
+//For loop example
+//initialization & increment is optional
+func forLoop() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+
+	fmt.Println(sum)
+}
+
+//simple for loop without init & condition is
+func whileLoop() {
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+
+	fmt.Println(sum)
+}
+
+//if else block example
+func powerFunction(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
+
+//Switch-Case example
+//In effect, the break statement that is needed at the end of each case in those languages is provided automatically in Go.
+//Another important difference is that Go's switch cases need not be constants, and the values involved need not be integers.
+func switchCase() {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
 }
